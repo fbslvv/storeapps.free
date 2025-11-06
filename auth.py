28eda088c00a0cddb5835b77736b1f94f9859c66
fbslvv <w7.fabin@gmail.com>
2025-11-06 15:14:50 -0300

    Corrige porta para Render

--- auth.py
+++ auth.py
@@ -174,5 +174,8 @@ def callback():
 def auth_route():
     return redirect("/login")
 
+import os
+
 if __name__ == "__main__":
-    app.run(debug=True)
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port)
